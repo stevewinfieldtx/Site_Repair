e6db639153dd2c88db6eff96062e1262597e39d5
stevewinfieldtx <stevewinfieldtx@gmail.com>
2026-01-18 17:03:46 -0600

    Implement "around to Blue" theme [Voice Command]

---
+++ src/app/site/page.tsx
@@ -0,0 +1,280 @@
+**ILLUSTORIA MAGAZINE**
+
+Young reader in your life? **Subscribe to Illustoria magazine today**. Recognized by [The New York Times](https://www.nytimes.com/) as a best gift for kids!
+
+This essay, which originally appeared in [The End of Trust], is included in this year’s Best American Essay’s notable list. In celebration, we’re publishing it here in full.
+
+- - -
+
+On a beach as a teenager, the artist Yves Klein and two friends decided, like Greek gods, to divide up the realms. One got the sea, another the earth, and Klein the sky. Here by the Mediterranean’s shores, he complained about the gulls stealing his vision, tearing through the azure overhead. “I began to feel hatred,” he wrote later, “for the birds which flew back and forth across my blue sky, cloudless sky, because they tried to bore holes in my greatest and most beautiful work.” Soon the sky became his domain. He went on to trademark a shade of blue. International Klein Blue was modeled on lapis lazuli. In 1957 he declared that we’d entered the Époque Bleue, the blue age. I think of him now, claiming the sky, hating the birds, calling that blue above his best work, as I consider different skies, different blues, different realms. These too are divvied up and doled out. They appear on the internet and our computers and phones, where blue is the most common color, as if that blue, the époque bleue, has only just dawned.
+
+I started pondering this blue age as an accident. I stared at my screen, a proverbial window, facing an actual window as I stood at my desk. I was writing, hence easily distracted. I noticed the icons for different apps lined up in the dock, as Apple calls it. Scrolling left to right, there was the Finder’s square smiley face, a cubist rendering in two blues, the face split like Picasso’s Demoiselles, shrunk to a half-inch square. The App Store, iChat, Mail, and Safari veered close to Klein’s luminescent lapis. Microsoft Word was turquoise with shadows in the folds of the W, as if to trick me into believing it had dimensionality, and the few apps that were white or gray took on a blue cast from the LED screen. On the document I wrote in, the formatting was blue, the margins marked in blue. An hour later I started an email, and the recipients’ names came in two different blues: a pale one indicating the To line, while, as I typed the email address, Mail helpfully offered a list of names in navy to jog my memory. And online, links were blue. I returned to my writing but couldn’t escape the blues. I highlighted text to delete it and start my sentence over, and this, too, was the same aching blue of a winter’s sky in the Catskills, where I live.
+
+I tried to work, told myself not to think of these hues, but the blues did something to me. I couldn’t shake them. It was the spring of my mother’s death, and
+
+[css-images-4] Clarifying CSS gradient rendering for edge cases involving "longer hue" interpolation ·Issue #11381 · [w3c](https://github.com/w3c/csswg-drafts/issues/11381)
+[Skip to content]
+## Navigation Menu
+Toggle navigation
+[]
+[Sign in]
+Appearance settings
+Search or jump to...
+# Search code, repositories, users, issues, pull requests...
+
+Search
+Clear
+[Search syntax tips]
+# Provide feedback
+
+We read every piece of feedback, and take your input very seriously.
+Include my email address so I can be contacted
+CancelSubmit feedback
+# Saved searches
+## Use saved searches to filter your results more quickly
+
+Name
+Query
+To see all available qualifiers, see our[documentation].
+CancelCreate saved search
+[Sign in]
+[Sign up]
+Appearance settings
+Resetting focus
+You signed in with another tab or window.[Reload] to refresh your session.You signed out in another tab or window.[Reload] to refresh your session.You switched accounts on another tab or window.[Reload] to refresh your session.Dismiss alert
+{{ message }}
+[w3c] /**[csswg-drafts] **Public
+* [Notifications] You must be signed in to change notification settings
+* [Fork765]
+* [Star4.8k]
+# [css-images-4] Clarifying CSS gradient rendering for edge cases involving "longer hue" interpolation#11381
+[New issue]
+Copy link
+[New issue]
+Copy link
+Closed
+Closed
+[[css-images-4] Clarifying CSS gradient rendering for edge cases involving "longer hue" interpolation] #11381
+Copy link
+Assignees
+[![LeaVerou]]
+Labels
+[Closed Accepted as Editorial] [css-images-4]
+[![@jfkthame]]
+## Description
+[![@jfkthame]]
+[jfkthame]
+opened[onDec 16, 2024]
+Issue body actions
+I am seeing differences between Firefox, Chrome and Safari regarding how gradients with the "longer hue" interpolation method are rendered. I think I understand what the spec expects, but none of the browsers tested get this right, so I'd like to confirm my interpretation —and perhaps some clarification or additional examples may be needed in the spec.
+Testcase:[https://codepen.io/jfkthame/pen/azopZOx]
+(Note that there have been some recent patches to gradient rendering in Firefox. The description below refers to current Nightly builds; older versions have some different and less self-consistent results.)
+This renders a number of 100px-wide gradient swatches, all of them defined as linear-gradient(to right in hsl longer hue, ...) with two color stops, red and blue, at a variety of positions. (The red and blue tick-marks above and below each swatch indicate the positions of the color stops defining the gradient.)
+Results I'm seeing in Firefox Nightly, Chrome Canary, and Safari Tech Preview:
+[![image]]
+The simplest case is (a), with the red stop at the left edge and the blue stop at the right. No issues here: we simply render a gradient from red to blue, going "the long way around" the HSL w
+
+Perpetuity Blues **a novelette** _by_ **Neal Barrett, Jr** On Maggie's seventh birthday, she found the courage to ask Mother what had happened to her father. "Your father disappeared under strange circumstances," said Mother. "Sorghumdances?" said Maggie. "Circumstances," said Mother, who had taught remedial English before marriage and was taking a stab at it again. "Circumstances: a condition or fact attending an event or having some bearing upon it." "I see," said Maggie. She didn't, but knew it wasn't safe to ask twice. What happened was Daddy got up after supper one night and put on his cardigan with the patches on the sleeves and walked to the 7-Eleven for catfood and bread. Eight months later, he hadn't shown up or called or written a card. Strange circumstances didn't seem like a satisfactory answer. Mother died Thursday afternoon. Maggie found her watching reruns of _Rawhide_ and _Bonanza_. Maggie left South Houston and went to live with Aunt Grace and Uncle Ned in Marble Creek. "There's no telling who he might of met at that store," said Aunt Grace. "Your father wasn't right after the service. I expect he got turned in Berlin. Sent him back and planted him deep in Montgomery Ward's as a mole. That's how they do it. You wait and lead an ordinary life. You might be anyone at all. Your control phones up one day and says 'the water runs deep in Lake Ladoga' and that's it. Whatever you're doing, you just get right up and do their bidding. Either that or he run off with that slut in appliance. I got a look at her when your uncle went down to buy the Lawnboy at the End-of-Summer Sale. Your mother married beneath her. I don't say I didn't do the same. The women in our family got no sense at all when it comes to men. We come from good stock, but that doesn't put money in the bank. Your grandfather Jack worked directly with the man who invented the volleyball net they use all over the world in tournament play. Of course he never got the credit he deserved. This family's rubbed elbows with greatness more than once, but you wouldn't know it. Don't listen to your Uncle Ned's stories. And for Christ's sake, don't ever sit on his lap." Maggie found life entirely different in a small town. There were new customs to learn. Jimmy Gerder and two other fourth graders took her down to the river after school and tried to make her take off her pants. Maggie didn't want to and ran home. After that, she ran home every day. Uncle Ned told her stories. Maggie learned why it wasn't a good idea to sit on his lap. "There was this paleontologist," said Uncle Ne | [Perpetuity Blues](http://www.infinityplus.co.uk/stories/perpetuityblues.htm)
+
+| |
+| --- | --- |
+| [infinity plus - sf, fantasy and horror] | [fiction] |
+| [infinity plus home page] [fiction] [non-fiction] [other stuff] [a to z] | |
+| | |
+| | |
+
+**a novelette** _by_ **Neal Barrett, Jr**
+
+On Maggie's seventh birthday, she found the courage to ask Mother what had happened to her father. "Your father disappeared under strange circumstances," said Mother. "Sorghumdances?" said Maggie. "Circumstances," said Mother, who had taught remedial English before marriage and was taking a stab at it again. "Circumstances: a condition or fact attending an event or having some bearing upon it." "I see," said Maggie. She didn't, but knew it wasn't safe to ask twice. What happened was Daddy got up after supper one night and put on his cardigan with the patches on the sleeves and walked to the 7-Eleven for catfood and bread. Eight months later, he hadn't shown up or called or written a card. Strange circumstances didn't seem like a satisfactory answer. Mother died Thursday afternoon. Maggie found her watching reruns of _Rawhide_ and _Bonanza_. Maggie left South Houston and went to live with Aunt Grace and Uncle Ned in Marble Creek. "There's no telling who he might of met at that store," said Aunt Grace. "Your father wasn't right after the service. I expect he got turned in Berlin. Sent him back and planted him deep in Montgomery Ward's as a mole. That's how they do it. You wait and lead an ordinary life. You might be anyone at all. Your control phones up one day and says 'the water runs deep in Lake Ladoga' and that's it. Whatever you're doing, you just get right up and do their bidding. Either that or he run off with that slut in appliance. I got a look at her when your uncle went down to buy the Lawnboy at the End-of-Summer Sale. Your mother married beneath her. I don't say I didn't do the same. The women in our family got no sense at all when it comes to men. We come from good stock, but that doesn't put money in the bank. Your grandfather Jack worked directly with the man who invented the volleyball net they use all over the world in tournament play. Of course he never got the credit he deserved. This family's rubbed elbows with greatness more than once, but you wouldn't know it. Don't listen to your Uncle Ned's stories. And for Christ's sake, don't ever sit on his lap." Maggie found life entirely different in a small town. There were new customs to learn. Jimmy Gerder and two other fourth graders took her down to the river after school and tried to make her take off her pants. Maggie didn't want to and ran home. After that, she ran home every day. Uncle Ned told her stories. Maggie learned why it wasn't a good idea to sit on his lap. "There was this paleontologist," said Uncle Ne
+
+Est. 1828
+[Dictionary] Definition
+
+- [adverb]
+- [preposition]
+- [adjective]
+- [adverb]
+- [preposition]
+- [adjective]
+- [Synonyms]
+- [Example Sentences]
+- [Word History]
+- [Phrases Containing]
+- [Entries Near]
+
+- - [Cite this Entry]
+- [Share]
+- [Kids Definition]
+- [More from M-W]
+
+[Show more]
+- [Show more]
+- [Citation]
+- [Share]
+- [Kids]
+- [More from M-W]
+
+Save Word
+To save this word, you'll need to log in.
+[Log In]
+
+# around
+
+1 of 3
+
+## [adverb]
+
+[ə-ˈrau̇nd]
+
+[Synonyms of _around]
+
+1
+
+a
+**: in a circle or in circumference**
+The wheel goes around.
+
+a tree five feet around
+
+b
+**: in, along, or through a circuit**
+The road goes around by the lake.
+
+2
+
+a
+**: on all or various sides **: in every or any direction**
+papers lying around
+
+There was nothing for miles around.
+
+b
+**: in close from all sides so as to surround**
+People crowded around.
+
+c
+**: in or near one's present place or situation**
+We had to wait around awhile.
+
+3
+
+a
+**: here and there **: from one place to another**
+She travels around on business.
+
+b
+**: to a particular place**
+You should come around for dinner.
+
+c
+—used with some verbs to indicate repeated or continued action
+He's always joking around when he should be serious.
+
+Don't play around with your food.
+
+4
+
+a
+**: in rotation or succession**
+Another winter has come around.
+
+b
+**: from beginning to end **: [through]**
+The weather is mild the year around.
+
+c
+**: in order**
+the other way around
+
+5
+
+**: in or to an opposite direction or position**
+turn around
+
+6
+
+**: with some approach to exactness **: [approximately]**
+It costs around $5.
+
+around
+
+2 of 3
+
+## [preposition]
+
+1
+
+a
+**: on all sides of**
+a yard with a fence around it
+
+b
+**: so as to encircle or enclose**
+seated around the table
+
+c
+**: so as to avoid or get past **: on or to another side of**
+trying to find a way around their objections
+
+We went around the lake.
+
+around the corner
+
+d
+**: [near]**
+lives around Chicago
+
+around the turn of the century
+
+2
+
+**: in all directions outward from**
+Look around you.
+
+3
+
+**: here and there in or throughout**
+They went barnstorming around the country.
+
+4
+
+**: so as to have a center or basis in**
+a society organized around kinship ties
+
+around
+
+3 of 3
+
+## [adjective]
+
+1
+
+**: [aboutsense 1]**
+He has been up and around for two days.
+
+2
+
+**: being in existence, evidence, or circulation**
+… the most intelligent of the artists around today.— R. M. Coates
+
+Phrases
+
+been around
+
+**: gone through many varied experiences **: become worldly-wise**
+He knows what he's talking about; he's _been around_.
+
+## Synonyms
+
+Adverb
+
+- [about]
+- [round]
+
+Preposition
+
+- 
+
+[Merriam-Webster](https://www.merriam-webster.com/dictionary/around?amp;utm_medium=serp&amp;utm_source=jsonld)
+
+[Merriam-Webster](https://www.merriam-webster.com/dictionary/around)
